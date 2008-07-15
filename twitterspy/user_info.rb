893531ec7c8c22068bb0bd100c01d3933fa5a9de
Dustin Sallings <dustin@spy.net>
2008-07-14 23:04:11 -0700

    Put the private message handling in its own method.

--- twitterspy/user_info.rb
+++ twitterspy/user_info.rb
@@ -12,16 +12,20 @@ module TwitterSpy
 
     def update(user)
       TwitterSpy::Threading::IN_QUEUE << Proc.new do
-        first_time = user.direct_message_id.nil?
-        twitter = twitter_conn user
-        # TODO:  Fix the twitter API to let me pass in my direct message ID
-        msgs = twitter.direct_messages.select{|m| m.id.to_i > user.direct_message_id}
-        user.update_attributes(:direct_message_id => msgs.first.id.to_i) if msgs.size > 0
-        deliver_messages(:private, user, msgs) unless first_time
+        do_private_messages user
       end
     end
 
     private
+
+    def do_private_messages(user)
+      first_time = user.direct_message_id.nil?
+      twitter = twitter_conn user
+      # TODO:  Fix the twitter API to let me pass in my direct message ID
+      msgs = twitter.direct_messages.select{|m| m.id.to_i > user.direct_message_id}
+      user.update_attributes(:direct_message_id => msgs.first.id.to_i) if msgs.size > 0
+      deliver_messages(:private, user, msgs) unless first_time
+    end
 
     def deliver_messages(type, user, msgs)
       msgs.each do |msg|
