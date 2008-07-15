81ba3c3f5345b3e5ff9cfecc97f43ef60f8d7bf1
Dustin Sallings <dustin@spy.net>
2008-07-14 21:49:12 -0700

    Model support for tracking a friend timeline.

--- twitterspy/models.rb
+++ twitterspy/models.rb
@@ -16,6 +16,7 @@ class User
   property :min_id, Integer, :nullable => false, :default => 0
   property :language, String, :length => 2
   property :auto_post, Boolean, :nullable => false, :default => false
+  property :friend_timeline_id, Integer
 
   has n, :user_tracks
   has n, :tracks, :through => :user_tracks
