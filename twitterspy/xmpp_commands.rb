a090f5cf4308a0b97281b14c9a9752f3ae984dbd
Dustin Sallings <dustin@spy.net>
2008-10-04 23:30:15 -0700

    Added base command for common "add field" pattern.

--- twitterspy/xmpp_commands.rb
+++ twitterspy/xmpp_commands.rb
@@ -37,6 +37,13 @@ module TwitterSpy
         actions.each {|a| ael.add_element a}
       end
 
+      def add_field(form, name, label, value=nil, type=:text_single)
+        ufield = form.add_element(Jabber::Dataforms::XDataField.new(name, type))
+        ufield.label = label
+        ufield.value = value.to_s unless value.nil?
+        ufield
+      end
+
       def execute(conn, user, iq)
         case iq.command.action
         when :cancel
