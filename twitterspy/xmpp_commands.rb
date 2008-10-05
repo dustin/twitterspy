178fb5643134480070bb17345cb53e99adf7ea0c
Dustin Sallings <dustin@spy.net>
2008-10-04 22:52:26 -0700

    Added untrack ad hoc command.

--- twitterspy/xmpp_commands.rb
+++ twitterspy/xmpp_commands.rb
@@ -29,6 +29,39 @@ module TwitterSpy
       end
     end
 
+    class MultiBase < Base
+
+      def next_actions(com, execute, *actions)
+        ael = com.add_element("actions")
+        ael.attributes['execute'] = execute
+        actions.each {|a| ael.add_element a}
+      end
+
+      def execute(conn, user, iq)
+        case iq.command.action
+        when :cancel
+          send_result(conn, iq, :canceled)
+        when nil, :complete
+          args = iq.command.first_element('x')
+          if args.blank?
+            send_result(conn, iq, :executing) do |com|
+              add_form(user, iq, com)
+            end
+          else
+            complete(conn, user, iq, args)
+          end
+        end
+      rescue
+        send_result(conn, iq) do |com|
+          note = com.add_element('note')
+          note.attributes['type'] = 'error'
+          note.add_text($!.to_s)
+          error = com.add_element(Jabber::ErrorResponse.new('bad-request'))
+        end
+      end
+
+    end
+
     class Version < Base
 
       def initialize
@@ -46,8 +79,36 @@ module TwitterSpy
 
     end
 
+    class UnTrack < MultiBase
+
+      def initialize
+        super('untrack', 'Remove a Track', 'Remove a track query.')
+      end
+
+      def add_form(user, iq, com)
+        next_actions(com, 'execute', 'complete')
+
+        form = com.add_element(Jabber::Dataforms::XData.new)
+        form.title = 'Untrack one or more current tracks.'
+        form.instructions = "Select the queries to stop tracking and submit."
+        field = form.add_element(Jabber::Dataforms::XDataField.new('torm', :list_multi))
+        field.label = 'Tracks'
+        field.options = user.tracks.sort_by{|t| t.query}.map{|t| [t.id, t.query]}
+      end
+
+      def complete(conn, user, iq, args)
+        torm = args.fields.select {|f| f.var == 'torm'}.first
+        torm.values.each do |i|
+          puts "Untracking #{i}"
+          user.untrack i.to_i
+        end
+        send_result(conn, iq)
+      end
+
+    end
+
     def self.commands
-      constants.map{|c| const_get c}.select {|c| c != Base }
+      constants.map{|c| const_get c}.select {|c| c != Base && c != MultiBase }
     end
   end
 
