0e1fe0e2246e95667cffb4cba5d6fb2a4db4675b
Dustin Sallings <dustin@spy.net>
2009-02-23 08:38:10 -0800

    Configurable DB types.

--- lib/twitterspy/db.py
+++ lib/twitterspy/db.py
@@ -1,2 +1,10 @@
-# Ideally, this will do some kind of selection to find the right DB.
-from db_couch import *
+import ConfigParser
+
+import config
+
+db_type = config.CONF.get("db", "type")
+
+if db_type == 'couch':
+    from db_couch import *
+else:
+    raise ConfigParser.Error("Unknown database type:  " + db_type)
