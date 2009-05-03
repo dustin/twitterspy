6b0944b956b1e5d58d551127666299ce5e6c8eaf
Dustin Sallings <dustin@spy.net>
2009-05-02 21:38:32 -0700

    Make PingHandler an IQHandlerMixin

--- lib/twitterspy/xmpp_ping.py
+++ lib/twitterspy/xmpp_ping.py
@@ -2,15 +2,15 @@ from zope.interface import implements
 
 from twisted.python import log
 from twisted.internet import defer
-from wokkel.subprotocols import XMPPHandler
+from wokkel.subprotocols import XMPPHandler, IQHandlerMixin
 from wokkel import disco
 from wokkel import generic
 from wokkel.iwokkel import IDisco
 
 NS_PING = 'urn:xmpp:ping'
-PING = generic.IQ_GET + '/query[@xmlns="' + NS_PING + '"]'
+PING = generic.IQ_GET + '/ping[@xmlns="' + NS_PING + '"]'
 
-class PingHandler(XMPPHandler):
+class PingHandler(XMPPHandler, IQHandlerMixin):
     """
     XMPP subprotocol handler for Ping.
 
@@ -20,14 +20,15 @@ class PingHandler(XMPPHandler):
 
     implements(IDisco)
 
+    iqHandlers = {PING: 'onPing'}
+
     def connectionInitialized(self):
         super(PingHandler, self).connectionInitialized()
-        self.xmlstream.addObserver(PING, self.onPing)
+        self.xmlstream.addObserver(PING, self.handleRequest)
 
     def sendPingResponse(self, iq):
         response = toResponse(iq, "result")
         self.send(response)
-        iq.handled = True
 
     def onPing(self, iq):
         log.msg("Got ping from %s" % iq.getAttribute("from"))
